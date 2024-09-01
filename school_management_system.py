61e889d59209a4263ff75afa96c01692bb08d5e8
daniyaltayyab <daniyaltayyab91@gmail.com>
2024-09-01 13:20:26 +0500

    Delete function added

--- school_management_system.py
+++ school_management_system.py
@@ -10,10 +10,10 @@ def get_all_students():
         return
 
     print("\nPrinting list of students: \n")
+    i = 1
     for student in students:
-        i = 1
         print(f"Record: {i}\nName: {student['name']}, Age: {student['age']}, Grade: {student['grade']}")
-        i += 1
+        i = i + 1
     print("\n")
 
 def add_student(name, age, grade):
@@ -32,10 +32,21 @@ def add_student(name, age, grade):
 def search_student_by_name(name):
     """Find and display a student by name."""
     for student in students:
-        if student['name'] == name:
+        if student['name'].lower() == name.lower():
             print(f"\nName: {student['name']}, Age: {student['age']}, Grade: {student['grade']}\n")
             return
     print(f"\nRecord not found against student name {name}\n")
+
+def delete_student_by_name(name):
+    """delete a student by name for the list"""
+    global students # Ensure that we are modifying the global students list
+    for student in students:
+        if student['name'].lower() == name.lower():
+            students.remove(student)
+            print(f"\nStudent with name {student} is deleted successfully\n")
+            return
+    print(f"\nNo record found against student name: {name}\n")
+
 
 def main():
     """Main function to run the school management system."""
@@ -44,9 +55,10 @@ def main():
         print("1. Add new student")
         print("2. View all students")
         print("3. Search student by name")
-        print("4. Exit the system")
+        print("4. Delete student by name")
+        print("5. Exit the system")
         print("################################################\n")
-        choice = int(input("Enter your choice (1/4): "))
+        choice = int(input("Enter your choice (1/5): "))
 
         if choice == 1:
             name = input("Enter student's name: ")
@@ -67,11 +79,15 @@ def main():
             search_student_by_name(name)
 
         elif choice == 4:
+            name = input("Enter student's name to delete: ")
+            delete_student_by_name(name)
+
+        elif choice == 5:
             print("Exiting the system, Goodbye!")
             break
 
         else:
-            print("Invalid choice, please select a number between 1 to 4")
+            print("Invalid choice, please select a number between 1 to 5")
 
 # Run the system by calling the main function
 if __name__ == "__main__":
